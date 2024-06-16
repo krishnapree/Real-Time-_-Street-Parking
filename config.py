10cd0bccc252a46e17d0678922c054b0ad72eb44
T.Krishna Preetham <139627687+krishnapree@users.noreply.github.com>
2024-06-16 14:26:59 +0530

    config.py

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import os
 class Config:
     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'uploads')
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-    TWILIO_ACCOUNT_SID = 'your_twilio_account_sid'
-    TWILIO_AUTH_TOKEN = 'your_twilio_auth_token'
+    TWILIO_ACCOUNT_SID = 'AC723ff75eb26c998305921ba3386d4806'
+    TWILIO_AUTH_TOKEN = '8b6001f18e6227baf9449d346a750130'
     TWILIO_PHONE_NUMBER = 'your_twilio_phone_number'
-    DYNAMIC_PRICING_BASE = 50  # Base price for parking
